dee7b75afdee3351941a4e5e0b142ca200bcf3ac
Dmitri <dima-189@mail.ru>
2017-09-02 23:19:35 +0300

    Procedure methods

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 5;
+            int y1 = 9;
+            char sim1 = '#';
+
+            Draw(x1, y1, sim1);
+
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
         }
     }
 }
