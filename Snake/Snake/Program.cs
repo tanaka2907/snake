8ecd1723aab5d6a1fdeef3ee0cdb1084334f86f3
Dmitri <dima-189@mail.ru>
2017-09-03 12:38:32 +0300

    LIST

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,22 @@ namespace Snake
             Point p2 = new Point(1,6,'*');
             p2.Draw();
 
+            Point p3 = new Point(7, 6, '*');
+            Point p4 = new Point(9, 10, '*');
+
+            List<Point> plist = new List<Point>;
+            plist.Add(p1);
+            plist.Add(p2);
+            plist.Add(p3);
+            plist.Add(p4);
+
+            List<char> clist = new List<char>;
+
+            clist.Add('#');
+            clist.Add('$');
+            clist.Add('@');
+
+
             Console.ReadLine();
         }
 
